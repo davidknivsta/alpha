381aa246ab044feedde846634ad193209c10ebe1
David Hemmingsson <david.uppsala@gmail.com>
2026-02-01 20:50:03 +0100

    Update debug variable for normal operation
    Change debug variable to false for normal behavior.

--- main.py
+++ main.py
@@ -53,7 +53,8 @@ def is_within_target_time():
     
     time_diff = abs(current_minutes - target_minutes)
 
-    debug = True #ändra till false för att få tidstest
+    #debug = True #kan köra alla tider för test
+    debug = False #normalbeteende
     
     if time_diff <= 40 or debug:
         print("✅ Inom tidsramen - kör!")
